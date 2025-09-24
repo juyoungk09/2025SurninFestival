02f4d4b14731552ffeed0e4cfdc1597374b92d82
juyoungk09 <juyoungk09@gmail.com>
2025-09-25 06:17:31 +0900

    eslin off

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com'], //허용할 외부 이미지 호스트 추가
   },
+  eslint: {
+    ignoreDuringBuilds: true, // 빌드 시 ESLint 체크 무시
+  },
 };
 
 export default nextConfig;
